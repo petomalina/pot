a24f1c57e670720c8b43e27739df056000b48433
Peter Malina <peter.malina@kiwi.com>
2023-12-18 19:30:40 +0100

    docs: add full example for elections

---
+++ examples/election/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/petomalina/pot"
+)
+
+type Leader struct {
+	ID string `json:"id"`
+}
+
+func (l Leader) Key() string {
+	return "leader"
+}
+
+func main() {
+	id := os.Getenv("ID")
+	if id == "" {
+		panic("ID is required")
+	}
+
+	ctx, done := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer done()
+
+	client := pot.NewClient[Leader]("http://localhost:8080")
+
+	primary := false
+	for {
+		slog.Info("attempting election", slog.String("id", id), slog.Bool("primary", primary))
+		res, err := client.Create("test/election", []Leader{{ID: id}}, pot.WithNoRewrite(time.Second*10))
+		if err != nil {
+			if errors.Is(err, pot.ErrNoRewriteViolated) {
+				primary = false
+			} else {
+				slog.Error("failed", slog.String("err", err.Error()))
+				primary = false
+			}
+
+			time.Sleep(time.Second * 3)
+		}
+
+		if !primary && err == nil {
+			primary = true
+			slog.Info("became primary", slog.String("id", id), slog.Int64("generation", res.Generation))
+		}
+
+		select {
+		case <-ctx.Done():
+			slog.Info("shutting down")
+			return
+		case <-time.After(time.Second * 5):
+		}
+	}
+}
